d9f0563c7b6977eb8b2c28359404b391d1f8a649
Santosh Thakur <santosh.thakur@electrolux.com>
2023-07-07 12:25:22 -0400

    deleted files

--- src/main/java/com/electrolux/pushnotification/registrationservice/mapper/BrandsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.electrolux.pushnotification.registrationservice.mapper;
-
-import com.electrolux.pushnotification.registrationservice.domain.Brands;
-//import com.electrolux.pushnotification.registrationservice.dto.BrandsDTO;
-import org.mapstruct.Mapper;
-
-//@Mapper(componentModel = "spring", uses = {})
-//public interface BrandsMapper extends EntityMapper<BrandsDTO, Brands> {
-//}
--- src/main/java/com/electrolux/pushnotification/registrationservice/repository/BrandsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.electrolux.pushnotification.registrationservice.repository;
-
-import com.electrolux.pushnotification.registrationservice.domain.Brands;
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-//public interface BrandsRepository extends JpaRepository<Brands, Integer> {
-//}
